85ff57d0a26ea92cf7dd64d3b6dcdb02fa9bf8fc
Luis Gallego Quero <lgaq94@gmail.com>
2019-01-02 19:47:54 +0100

    mas tela

--- DSS-P4/src/servidor/Db.java
+++ DSS-P4/src/servidor/Db.java
@@ -2,7 +2,9 @@
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Db {
 	String url = "jdbc:mysql://localhost:3306/consorcio";
@@ -15,9 +17,25 @@ public class Db {
 	public Db() throws SQLException, InstantiationException, IllegalAccessException, ClassNotFoundException {
 
 		Class.forName("com.mysql.jdbc.Driver").newInstance();
-		try {
+		
+		try {	// Conectamos
 			System.out.println("------CONECTANDO BD------");
 			this.connection = DriverManager.getConnection(url, username,password);
+			System.out.println("------BD CONECTADA------");
+			
+			/*
+			 * Statement: objeto utilizado para ejecutar una instrucción SQL estática y devolver los resultados que produce
+			 * Result: Recoge el resultado. Solo podemos tener uno por statement.
+			 */
+			System.out.println("------REALIZAMOS UNA PRUEBA------");
+			Statement statement = this.connection.createStatement();
+			ResultSet result = statement.executeQuery("SELECT * FROM farmacia;");
+			System.out.println("------FIN PRUEBA------");
+			
+			while(result.next()){
+				System.out.println(result.getString(1) + "   " + result.getString(2) + "   " +result.getString(3) + "   " +result.getString(4));
+			}
+			
 		}catch(SQLException ex) {
 			System.out.println("-----ERROR SQL-----");
 		    System.out.println("SQLException: " + ex.getMessage());
@@ -25,8 +43,7 @@ public class Db {
 		    System.out.println("VendorError: " + ex.getErrorCode());
 		} catch(Exception e){
 			System.out.println("-----ERROR CONEXIÓN BD-----");
-		}
-		
+		}		
 	}
 	public Connection getDB() {
 		return this.connection;
