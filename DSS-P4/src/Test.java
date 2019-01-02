2f24dd9c8de29266168cc4d51d49d81738b83f19
Luis Gallego Quero <lgaq94@gmail.com>
2019-01-02 18:51:04 +0100

    dale

--- DSS-P4/src/Test.java
+++ DSS-P4/src/Test.java
@@ -6,17 +6,21 @@ import com.sun.jersey.api.client.config.ClientConfig;
 import com.sun.jersey.api.client.config.DefaultClientConfig;
 import com.sun.jersey.api.json.JSONConfiguration;
 
+import servidor.Db2;
+
 public class Test {
 	
 	public static void main(String[] args){
 		
 		//Producto p = new Producto(9999, "producto json");
 		
-		ClientConfig clientConfig = new DefaultClientConfig();
+		/*ClientConfig clientConfig = new DefaultClientConfig();
 		clientConfig.getFeatures().put(JSONConfiguration.FEATURE_POJO_MAPPING, Boolean.TRUE);
 		Client client = Client.create(clientConfig);
 		
-		WebResource servicio = client.resource(UriBuilder.fromUri("http://localhost:8080/DSS-P4/rest").build());
+		WebResource servicio = client.resource(UriBuilder.fromUri("http://localhost:8080/DSS-P4/rest").build());*/
+		
+		Db2 bd = new Db2();
 		
 	}
 
